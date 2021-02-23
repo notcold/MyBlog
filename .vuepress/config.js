35581602ab6fb1db8150f89ad9e3822eba2b248c
剑来 <jianlai@tsign.cn>
2021-02-23 20:24:40 +0800

    fix: 数据迁移

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,108 +1,79 @@
 module.exports = {
-  "title": "my blog",
-  "description": "write something",
-  "dest": "public",
-  "configureWebpack": {
-    "resolve": {
-      "alias": {
-        '@static': 'static/'
-      }
-    }
+  title: "my blog",
+  description: "write something",
+  dest: "public",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@static": "static/",
+      },
+    },
   },
-  "head": [
+  head: [
     [
       "link",
       {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
+        rel: "icon",
+        href: "/favicon.ico",
+      },
     ],
     [
       "meta",
       {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [
+  theme: "reco",
+  themeConfig: {
+    nav: [
       {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
+        text: "Home",
+        link: "/",
+        icon: "reco-home",
       },
       {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
+        text: "TimeLine",
+        link: "/timeline/",
+        icon: "reco-date",
       },
       {
-        "text": "Docs",
-        "icon": "reco-message",
-        "items": [
+        text: "Contact",
+        icon: "reco-message",
+        items: [
           {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
-          }
-        ]
+            text: "GitHub",
+            link: "https://github.com/notcold",
+            icon: "reco-github",
+          },
+        ],
       },
-      {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "GitHub",
-            "link": "https://github.com/recoluan",
-            "icon": "reco-github"
-          }
-        ]
-      }
     ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
+    sidebar: {
+      "/docs/theme-reco/": ["", "theme", "plugin", "api"],
     },
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
+    type: "blog",
+    blogConfig: {
+      category: {
+        location: 2,
+        text: "Category",
       },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
-      }
+      tag: {
+        location: 3,
+        text: "Tag",
+      },
     },
-    "friendLink": [
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
-    ],
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "sk",
-    "authorAvatar": "/avatar.png",
-    "record": "xxxx",
-    "startYear": "2017"
+    friendLink: [],
+    logo: "/logo.png",
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: "Last Updated",
+    author: "sk",
+    authorAvatar: "/avatar.png",
+    startYear: "2017",
   },
-  "markdown": {
-    "lineNumbers": true
-  }
-}
+  markdown: {
+    lineNumbers: true,
+  },
+};
